d815c8024dfd9aada2ba7002d8b9aeebb4b6c1ea
Rico <rico0808@126.com>
2021-11-15 19:21:59 +0800

    fix

--- src/apis/config/config.default.ts
+++ src/apis/config/config.default.ts
@@ -1,17 +1,21 @@
 export const orm = {
   type: "mysql",
-  host: "116.62.149.125",
-  port: 4400,
-  username: "one",
-  password: "mzSGX2tXEdLEyNZA",
-  database: "one",
+  host: "127.0.0.1",
+  port: 3306,
+  username: "",
+  password: "",
+  database: "",
   synchronize: true,
   logging: false,
   timezone: "+08:00",
 };
 
-export const JwtSecret = "5b5c7dymqwed11dwwd24ud5de46khs";
+// jwt 秘钥
+export const JwtSecret = "";
 
-export const agisoToken = "TbAldsv4b6rxc57sm5ynvc4ef3c9kvg7fcavk4zee68egdy4nw";
-export const agisoSecret = "5b5c7dymubmmp7etg9yd24ud5de46khs";
-export const testGoods = "100001";
+// agiso发货
+export const agisoToken = "";
+export const agisoSecret = "";
+
+// 测试商品sku
+export const testGoods = "";
